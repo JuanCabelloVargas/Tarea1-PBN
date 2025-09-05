649ae99cca51b47739aee1ab5c0e823d10ad14b4
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-04 22:14:07 -0400

    feat: create simple number decoder

--- CABELLO-JUAN-tarea1.c
+++ CABELLO-JUAN-tarea1.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
-
-const char *morse_numbers[10] = {
-  "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." //0,1,2,3,4,5,6,7,8,9
+const char *MORSE_DIGITS[10] = {
+    "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..","----." //0,1,2,3,4,5,6,7,8,9 
 };
 
-
-
-int main(){
-  
+const char* morse_for_digit(char c) {
+    if (c >= '0' && c <= '9') {
+        return MORSE_DIGITS[c - '0'];
+    }
+    return NULL; 
 }
+
+int main() {
+    char input[100];  
+
+    printf("nuumero: ");
+    scanf("%99s", input);   
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        const char *num = morse_for_digit(input[i]);
+        if (num) {
+            printf("%s ", num); 
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
